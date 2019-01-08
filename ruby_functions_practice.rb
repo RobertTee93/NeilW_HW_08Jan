4bbb66956550752f8e34591e16eca6e9bb3c3100
Robert Templeton <roberttee1993@googlemail.com>
2019-01-08 17:12:35 +0000

    modified ruby function for months

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -35,66 +35,15 @@ def add_string_as_number (string_1, string_2)
 end
 
 def number_to_full_month_name(number)
-  case number
-   when 1
-     return "January"
-   when 2
-     return "February"
-   when 3
-     return "March"
-   when 4
-     return "April"
-   when 5
-     return "May"
-   when 6
-     return "June"
-   when 7
-     return "July"
-   when 8
-     return "August"
-   when 9
-     return "September"
-   when 10
-     return "October"
-   when 11
-     return "November"
-   when 12
-     return "December"
-   else
-     return "Please input a number between 1 & 12"
-  end
+  require "Date"
+  return Date::MONTHNAMES[number]
 end
 
 def number_to_short_month_name(number)
-  case number
-   when 1
-     return "Jan"
-   when 2
-     return "Feb"
-   when 3
-     return "Mar"
-   when 4
-     return "Apr"
-   when 5
-     return "May"
-   when 6
-     return "Jun"
-   when 7
-     return "Jul"
-   when 8
-     return "Aug"
-   when 9
-     return "Sep"
-   when 10
-     return "Oct"
-   when 11
-     return "Nov"
-   when 12
-     return "Dec"
-   else
-     return "Please input a number between 1 & 12"
-  end
+  require "Date"
+  return Date::ABBR_MONTHNAMES[number]
 end
+
 
 def volume_of_cube(length)
   return length ** 3
